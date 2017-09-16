edf7411f1376e26b3645c578db5463eea18ce5ce
Your <ammarmousa96@gmail.com>
2017-09-16 02:23:54 +0200

    pip

--- media.py
+++ media.py
@@ -4,16 +4,14 @@ import webbrowser
 class Movie():
 
 
-    def __init__(self,title,story,poster_image,trailer):
+   def __init__(self, title, story, poster_image, trailer):
         """Make Constructor"""
-        self.title=title
-        self.storyline=story
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer
+        self.title = title
+        self.storyline = story
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer
 
 
-    def trailer(self):
+   def trailer(self):
         webbrowser.open(self.trailer_youtube_url)
         """method to open trailer on browser"""
-
-
